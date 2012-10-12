3c508660642665314715c2453e64bb130ee3b6c2
Kevin J. Woolley <kjw@pathological.ca>
2012-10-11 23:55:37 -0700

    Start things off with a DirectoryStream exercise.

---
+++ DirectoryStream1.java
@@ -0,0 +1,25 @@
+import java.nio.file.*;
+
+
+class DirectoryStream1 {
+    public static void main(String[] args)
+    {
+        if (args.length == 0) {
+            System.err.println("Need directory argument.");
+            System.exit(1);
+        }
+
+        Path dirpath = FileSystems.getDefault().getPath(args[0]);
+
+        try (DirectoryStream<Path> dirstream
+                = Files.newDirectoryStream(dirpath)) {
+            for (Path file: dirstream) {
+                System.out.println(file.getFileName());
+            }
+        }
+        catch (Exception e) {
+            System.err.println(e);
+        }
+    }
+}
+
